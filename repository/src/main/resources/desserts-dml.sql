f90fbee78c7c601f7795ea67683b3bafdcd61680
Tomasz Bułat <buatss@gmail.com>
2022-11-16 12:34:29 +0100

    add desserts-dml.sql

---
+++ repository/src/main/resources/desserts-dml.sql
@@ -0,0 +1,4 @@
+INSERT INTO desserts (name, price) VALUES ('Ice cream', '3.99')
+INSERT INTO desserts (name, price) VALUES ('Waffles', '4.99')
+INSERT INTO desserts (name, price) VALUES ('Pancakes', '4.99')
+INSERT INTO desserts (name, price) VALUES ('Chocolate Whip', '6.99')
