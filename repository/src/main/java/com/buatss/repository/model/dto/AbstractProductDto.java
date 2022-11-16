b9c1ec5ead14097c3167e68b1d633adaccef83b0
Tomasz Bułat <buatss@gmail.com>
2022-11-16 10:36:06 +0100

    add overridden toString()

--- repository/src/main/java/com/buatss/repository/model/dto/AbstractProductDto.java
+++ repository/src/main/java/com/buatss/repository/model/dto/AbstractProductDto.java
@@ -1,26 +1,26 @@
 package com.buatss.repository.model.dto;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
-@ToString
-@EqualsAndHashCode
 public abstract class AbstractProductDto<T extends AbstractProductDto<T>> implements Comparable<T> {
-    private Integer id;
+    protected Integer id;
 
-    private String name;
+    protected String name;
 
-    private BigDecimal price;
+    protected BigDecimal price;
 
     @Override
     public int compareTo(T o) {
         AbstractProductDto<T> product = o;
         return this.getName().compareTo(product.name);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractProductDto{" + "id=" + id + ", name='" + name + '\'' + ", price=" + price + '}';
     }
 }
