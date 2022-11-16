a2589172798ca2af7bc80499e64f33fb6005839e
Tomasz Bułat <buatss@gmail.com>
2022-11-16 14:32:34 +0100

    [s] OrderRepository replace page finding by add method

--- repository/src/main/java/com/buatss/repository/dao/OrderRepository.java
+++ repository/src/main/java/com/buatss/repository/dao/OrderRepository.java
@@ -1,12 +1,12 @@
 package com.buatss.repository.dao;
 
 import com.buatss.repository.model.dto.OrderDto;
+import com.buatss.repository.model.entity.OrderEntity;
 
 import java.util.Optional;
-import java.util.TreeSet;
 
 public interface OrderRepository {
     Optional<OrderDto> findById(int id);
 
-    Optional<TreeSet<OrderDto>> findPageById(int pageNumber, int pageSize);
+    Optional<OrderDto> addOrder(OrderEntity order);
 }
