bce6200792accd7f3e7e25b100a382ab7ec9b9fb
Tomasz Bułat <buatss@gmail.com>
2022-11-16 11:27:51 +0100

    add drinks converting methods

--- repository/src/main/java/com/buatss/repository/model/converter/EntityDtoConverter.java
+++ repository/src/main/java/com/buatss/repository/model/converter/EntityDtoConverter.java
@@ -1,6 +1,8 @@
 package com.buatss.repository.model.converter;
 
+import com.buatss.repository.model.dto.DrinkDto;
 import com.buatss.repository.model.dto.MealDto;
+import com.buatss.repository.model.entity.DrinkEntity;
 import com.buatss.repository.model.entity.MealEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +22,23 @@ public class EntityDtoConverter {
         this.modelMapper = modelMapper;
     }
 
-    public MealDto convertEntityToDto(MealEntity entity){
+    public MealDto convertEntityToDto(MealEntity entity) {
         return modelMapper.map(entity, MealDto.class);
+    }
+
+    public DrinkDto convertEntityToDto(DrinkEntity entity) {
+        return modelMapper.map(entity, DrinkDto.class);
     }
 
     public TreeSet<MealDto> convertMealEntityListToMealDtoTreeSet(List<MealEntity> entities) {
         return entities.stream()
                        .map(entity -> modelMapper.map(entity, MealDto.class))
                        .collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparingLong(MealDto::getId))));
+    }
+
+    public TreeSet<DrinkDto> convertDrinkEntityListToDrinkDtoTreeSet(List<DrinkEntity> entities) {
+        return entities.stream()
+                       .map(entity -> modelMapper.map(entity, DrinkDto.class))
+                       .collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparingLong(DrinkDto::getId))));
     }
 }
