c960d4fc2cbcc9445e4304df367b398ced9993de
Tomasz Bułat <buatss@gmail.com>
2022-11-18 12:10:48 +0100

    add OrderService

---
+++ service/src/main/java/com/buatss/service/logic/OrderService.java
@@ -0,0 +1,9 @@
+package com.buatss.service.logic;
+
+import com.buatss.repository.model.dto.OrderDto;
+import com.buatss.repository.model.entity.OrderEntity;
+
+public interface OrderService {
+    //This should sum all product prices and delegate it to repository to persist
+    OrderDto createOrder(OrderEntity order);
+}
