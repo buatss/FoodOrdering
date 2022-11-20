663219f40dbde076f10de569f52713903438173c
Tomasz Bułat <buatss@gmail.com>
2022-11-20 18:07:09 +0100

    add all DrinkExtras to each cuisine

--- service/src/main/java/com/buatss/service/impl/CuisineServiceImpl.java
+++ service/src/main/java/com/buatss/service/impl/CuisineServiceImpl.java
@@ -1,7 +1,9 @@
 package com.buatss.service.impl;
 
 import com.buatss.repository.dao.CuisineRepository;
+import com.buatss.repository.dao.DrinkExtrasRepository;
 import com.buatss.repository.model.dto.CuisineDto;
+import com.buatss.repository.model.dto.DrinkExtrasDto;
 import com.buatss.service.CuisineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,18 +14,23 @@ import java.util.TreeSet;
 @Service
 public class CuisineServiceImpl implements CuisineService {
 
-    private final CuisineRepository repository;
+    private final CuisineRepository cuisineRepository;
+
+    private final DrinkExtrasRepository drinkExtrasRepository;
 
     @Autowired
-    public CuisineServiceImpl(CuisineRepository repository) {
-        this.repository = repository;
+    public CuisineServiceImpl(CuisineRepository repository, DrinkExtrasRepository drinkExtrasRepository) {
+        this.cuisineRepository = repository;
+        this.drinkExtrasRepository = drinkExtrasRepository;
     }
 
     @Override
     public TreeSet<CuisineDto> getCuisines() {
-        Optional<TreeSet<CuisineDto>> result = repository.findPageById(1, 5);
-        if (result.isPresent()) {
-            return result.get();
+        Optional<TreeSet<CuisineDto>> cuisinesResult = cuisineRepository.findPageById(1, 5);
+        Optional<TreeSet<DrinkExtrasDto>> drinkExtrasResult = drinkExtrasRepository.findPageById(1, 5);
+        if (cuisinesResult.isPresent() && drinkExtrasResult.isPresent()) {
+            cuisinesResult.get().forEach(cuisine -> cuisine.setDrinkExtras(drinkExtrasResult.get()));
+            return cuisinesResult.get();
         } else {
             throw new RuntimeException("No resources found");
         }
