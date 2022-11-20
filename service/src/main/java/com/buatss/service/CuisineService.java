a6c42b200680a320a8477f8e87ce0c08b282c5e8
Tomasz Bułat <buatss@gmail.com>
2022-11-20 18:06:41 +0100

    add comment

--- service/src/main/java/com/buatss/service/CuisineService.java
+++ service/src/main/java/com/buatss/service/CuisineService.java
@@ -5,5 +5,6 @@ import com.buatss.repository.model.dto.CuisineDto;
 import java.util.TreeSet;
 
 public interface CuisineService {
+    //This should also internally add all drink extras to each cuisine as long as they are common for all of them.
     TreeSet<CuisineDto> getCuisines();
 }
