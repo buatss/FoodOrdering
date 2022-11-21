c0f85819912c1069088690bdccbf53ca477f8161
Tomasz Bułat <buatss@gmail.com>
2022-11-21 15:45:45 +0100

    replace magic number by class-level constant

--- service/src/main/java/com/buatss/service/impl/CuisineServiceImpl.java
+++ service/src/main/java/com/buatss/service/impl/CuisineServiceImpl.java
@@ -18,6 +18,10 @@ public class CuisineServiceImpl implements CuisineService {
 
     private final DrinkExtrasRepository drinkExtrasRepository;
 
+    private final int DEFAULT_PAGE_NUMBER = 1;
+
+    private final int DEFAULT_PAGE_SIZE = 5;
+
     @Autowired
     public CuisineServiceImpl(CuisineRepository repository, DrinkExtrasRepository drinkExtrasRepository) {
         this.cuisineRepository = repository;
@@ -26,8 +30,10 @@ public class CuisineServiceImpl implements CuisineService {
 
     @Override
     public TreeSet<CuisineDto> getCuisines() {
-        Optional<TreeSet<CuisineDto>> cuisinesResult = cuisineRepository.findPageById(1, 5);
-        Optional<TreeSet<DrinkExtrasDto>> drinkExtrasResult = drinkExtrasRepository.findPageById(1, 5);
+        Optional<TreeSet<CuisineDto>> cuisinesResult =
+                cuisineRepository.findPageById(DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE);
+        Optional<TreeSet<DrinkExtrasDto>> drinkExtrasResult =
+                drinkExtrasRepository.findPageById(DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE);
         if (cuisinesResult.isPresent() && drinkExtrasResult.isPresent()) {
             cuisinesResult.get().forEach(cuisine -> cuisine.setDrinkExtras(drinkExtrasResult.get()));
             return cuisinesResult.get();
